422acab035c3b7c2d5b769c3c8ea3d3c2b15fb89
Amy Doo <amy30@berkeley.edu>
2019-10-05 19:33:19 -0700

    weather

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,11 @@ class MainController < ApplicationController
 
   def index
     # Uncomment and pass a parameter to the get function
-    # @w = WeatherService.get()
+    @w = WeatherService.get(params[:city])
+    if @w 
+      @temperature = ((@w[:temperature]-273) * 9.0 / 5 + 32).round(2)
+    end
+
   end
 
 end
