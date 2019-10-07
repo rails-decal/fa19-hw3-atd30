7d8136771649e4c3c16fc09ae2b8f5cad7a8ab46
Amy Doo <amy30@berkeley.edu>
2019-10-07 14:20:53 -0700

    lab done

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,11 +3,20 @@ require_relative '../services/weather_service'
 class MainController < ApplicationController
 
   def index
-    # Uncomment and pass a parameter to the get function
     @w = WeatherService.get(params[:city])
     if @w 
       @temperature = ((@w[:temperature]-273) * 9.0 / 5 + 32).round(2)
+      
+      city = City.new(
+      name: params[:city],
+      description: @w[:description],
+      temperature: @temperature
+    )
+
+    city.save
     end
+
+    
 
   end
 
